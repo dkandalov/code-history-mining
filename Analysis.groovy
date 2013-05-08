e050d5299543b67537ac02c5177f37ea11fb2c1d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-05-08 18:42:52 +0100

    moved word size adjustment into js

--- Analysis.groovy
+++ Analysis.groovy
@@ -93,19 +93,9 @@ class Analysis {
 			wordFrequency
 		}
 		def mostFrequentWords = wordOccurrences.entrySet().sort{ -it.value }.take(600)
-		println(mostFrequentWords.join("\n"))
 
-		def min = wordOccurrences.min{ it.value }.value
-		def max = wordOccurrences.max{ it.value }.value
-		def range = (max != min ? max - min : 1)
-		def adjustWordSize = { entry ->
-			def word = entry.key
-			def size = entry.value
-			if (word.size() < 5) size += (max * 0.3) // this is to make shorter words more noticeable
-			Math.round((double) 5 + ((size - min) * 75 / range))
-		}
 		"""{"words": [
-${mostFrequentWords.collect { '{"text": "' + it.key + '", "size": ' + adjustWordSize(it) + '}' }.join(",\n")}
+${mostFrequentWords.collect { '{"text": "' + it.key + '", "size": ' + it.value + '}' }.join(",\n")}
 ]}
 """
 	}
