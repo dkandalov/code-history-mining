46bd8682a8bd5dfb244a98c7d862b17e073e0116
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-05-15 19:16:08 +0100

    test for less than a day intervals

--- history/util/TimeIteratorsTest.groovy
+++ history/util/TimeIteratorsTest.groovy
@@ -4,6 +4,7 @@ import groovy.time.TimeCategory
 import org.junit.Test
 
 import static history.util.DateTimeUtil.date
+import static history.util.DateTimeUtil.dateTime
 
 class TimeIteratorsTest {
 	@Test void "should iterate from present to past in intervals"() {
@@ -31,6 +32,18 @@ class TimeIteratorsTest {
 		assert iterator.next() == [from: date("01/01/1970"), to: date("08/01/1970")]
 		assert iterator.hasNext()
 		assert iterator.next() == [from: date("08/01/1970"), to: date("11/01/1970")]
+		assert !iterator.hasNext()
+	}
+
+	@Test void "should work with less than a day interval"() {
+		def fromDate = dateTime("00:10 01/01/1970")
+		def toDate = dateTime("00:30 01/01/1970")
+		def stepSizeInDays = 2
+
+		def iterator = new PresentToPastIterator(fromDate, toDate, stepSizeInDays)
+
+		assert iterator.hasNext()
+		assert iterator.next() == [from: fromDate, to: toDate]
 		assert !iterator.hasNext()
 	}
 }
