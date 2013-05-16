efdc82e14f0de27b8041369d38ec09c10f9ebf16
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-05-16 20:59:54 +0100

    integration test for moved file

--- history/SourceOfChangeEventsTest.groovy
+++ history/SourceOfChangeEventsTest.groovy
@@ -1,25 +1,37 @@
 package history
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.vcs.changes.Change
 import org.junit.Test
 
+import static history.SourceOfChangeLists.requestChangeListsFor
 import static history.util.DateTimeUtil.dateTime
 
 class SourceOfChangeEventsTest {
-	private final jUnitProject
+	private final Project jUnitProject
 
 	SourceOfChangeEventsTest() {
 		jUnitProject = ProjectManager.instance.openProjects.find{it.name == "junit"}
 		assert jUnitProject != null : "Couldn't find open project with junit project (which is required for this test)"
 	}
 
-	@Test "should treat moved file as a single event"() {
-		def changeLists = SourceOfChangeLists.requestChangeListsFor(jUnitProject, dateTime("15:40 03/10/2007"), dateTime("15:45 03/10/2007"))
+	@Test "should treat renamed file as a single event"() {
+		def changeLists = requestChangeListsFor(jUnitProject, dateTime("15:40 03/10/2007"), dateTime("15:45 03/10/2007"))
 		def change = changeLists.first().changes.find{ it.beforeRevision.file.name.contains("TheoryMethod") }
 
 		assert change.type == Change.Type.MOVED
 		assert change.beforeRevision.file.name == "TheoryMethod.java"
 		assert change.afterRevision.file.name == "TheoryMethodRunner.java"
+	}
+
+	@Test "should treat moved file as a single event"() {
+		def changeLists = requestChangeListsFor(jUnitProject, dateTime("08:50 28/07/2011"), dateTime("09:00 28/07/2011"))
+		def change = changeLists.first().changes.find{ it.beforeRevision.file.name.contains("RuleFieldValidator") }
+
+		assert change.type == Change.Type.MOVED
+		assert change.beforeRevision.file.name == "RuleFieldValidator.java"
+		assert change.afterRevision.file.name == "RuleFieldValidator.java"
+
 	}
 }
