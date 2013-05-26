bf08d213bdb149c67a61b68ced47c5090839712b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-05-26 13:24:45 +0100

    refactoring; handling for FilesTooBigForDiffException

--- history/CommitFilesMunger.groovy
+++ history/CommitFilesMunger.groovy
@@ -1,15 +1,16 @@
 package history
+import com.intellij.openapi.diff.impl.ComparisonPolicy
 import com.intellij.openapi.diff.impl.highlighting.FragmentSide
 import com.intellij.openapi.diff.impl.processing.TextCompareProcessor
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.versionBrowser.CommittedChangeList
+import com.intellij.util.diff.FilesTooBigForDiffException
 import history.events.ChangeStats
 import history.events.FileChangeEvent
 import history.events.FileChangeInfo
 
-import static com.intellij.openapi.diff.impl.ComparisonPolicy.TRIM_SPACE
 import static com.intellij.openapi.diff.impl.util.TextDiffTypeEnum.*
 import static com.intellij.openapi.util.io.FileUtil.toCanonicalPath
 import static history.CommitMungingUtil.*
@@ -39,31 +40,9 @@ class CommitFilesMunger {
 		def nonEmptyRevision = nonEmptyRevisionOf(change)
 		if (nonEmptyRevision.file.fileType.binary) readFileContent = false
 
-		def lineChangesStats = FileChangeInfo.NA
-		def charChangesStats = FileChangeInfo.NA
+		def (lineChangesStats, charChangesStats) = [FileChangeInfo.NA, FileChangeInfo.NA]
 		if (readFileContent) {
-			def (beforeText, afterText) = (readFileContent ? contentOf(change) : ["", ""])
-			int linesBefore = beforeText.empty ? 0 : beforeText.split("\n").length
-			int linesAfter = afterText.empty ? 0 : afterText.split("\n").length
-			int charsBefore = beforeText.empty ? 0 : beforeText.length()
-			int charsAfter = afterText.empty ? 0 : afterText.length()
-
-			def fragmentsByType = new TextCompareProcessor(TRIM_SPACE).process(beforeText, afterText).groupBy{it.type}
-			if (!fragmentsByType.containsKey(INSERT)) fragmentsByType[INSERT] = []
-			if (!fragmentsByType.containsKey(CHANGED)) fragmentsByType[CHANGED] = []
-			if (!fragmentsByType.containsKey(DELETED)) fragmentsByType[DELETED] = []
-
-			int linesAdded = fragmentsByType[INSERT].sum(0){it.endLine2 - it.startingLine2}
-			int linesModified = fragmentsByType[CHANGED].sum(0){it.endLine1 - it.startingLine1}
-			int linesRemoved = fragmentsByType[DELETED].sum(0){it.endLine1 - it.startingLine1}
-
-			def charsInFragments = { lineFragments, fragmentSide -> lineFragments.sum(0){ it.getRange(fragmentSide).length } }
-			int charsAdded = charsInFragments(fragmentsByType[INSERT], FragmentSide.SIDE2)
-			int charsModified = charsInFragments(fragmentsByType[CHANGED], FragmentSide.SIDE1)
-			int charsRemoved = charsInFragments(fragmentsByType[DELETED], FragmentSide.SIDE1)
-
-			lineChangesStats = new ChangeStats(linesBefore, linesAfter, linesAdded, linesModified, linesRemoved)
-			charChangesStats = new ChangeStats(charsBefore, charsAfter, charsAdded, charsModified, charsRemoved)
+			(lineChangesStats, charChangesStats) = readChangeStats(change)
 		}
 
 		def projectPath = toCanonicalPath(project.basePath)
@@ -78,5 +57,36 @@ class CommitFilesMunger {
 				lineChangesStats,
 				charChangesStats
 		)
+	}
+
+	private static readChangeStats(Change change) {
+		def (beforeText, afterText) = contentOf(change)
+		int linesBefore = beforeText.empty ? 0 : beforeText.split("\n").length
+		int linesAfter = afterText.empty ? 0 : afterText.split("\n").length
+		int charsBefore = beforeText.empty ? 0 : beforeText.length()
+		int charsAfter = afterText.empty ? 0 : afterText.length()
+
+		def fragmentsByType = [:]
+		try {
+			fragmentsByType = new TextCompareProcessor(ComparisonPolicy.IGNORE_SPACE)
+					.process(beforeText, afterText).groupBy{ it.type }
+		} catch (FilesTooBigForDiffException ignored) {
+			return [FileChangeInfo.TOO_BIG_TO_DIFF, FileChangeInfo.TOO_BIG_TO_DIFF]
+		}
+		if (!fragmentsByType.containsKey(INSERT)) fragmentsByType[INSERT] = []
+		if (!fragmentsByType.containsKey(CHANGED)) fragmentsByType[CHANGED] = []
+		if (!fragmentsByType.containsKey(DELETED)) fragmentsByType[DELETED] = []
+
+		int linesAdded = fragmentsByType[INSERT].sum(0){it.endLine2 - it.startingLine2}
+		int linesModified = fragmentsByType[CHANGED].sum(0){it.endLine1 - it.startingLine1}
+		int linesRemoved = fragmentsByType[DELETED].sum(0){it.endLine1 - it.startingLine1}
+
+		def charsInFragments = { lineFragments, fragmentSide -> lineFragments.sum(0){ it.getRange(fragmentSide).length } }
+		int charsAdded = charsInFragments(fragmentsByType[INSERT], FragmentSide.SIDE2)
+		int charsModified = charsInFragments(fragmentsByType[CHANGED], FragmentSide.SIDE1)
+		int charsRemoved = charsInFragments(fragmentsByType[DELETED], FragmentSide.SIDE1)
+
+		[new ChangeStats(linesBefore, linesAfter, linesAdded, linesModified, linesRemoved),
+		new ChangeStats(charsBefore, charsAfter, charsAdded, charsModified, charsRemoved)]
 	}
 }
--- history/events/FileChangeInfo.groovy
+++ history/events/FileChangeInfo.groovy
@@ -5,6 +5,7 @@ import groovy.transform.Immutable
 @Immutable
 class FileChangeInfo {
 	static final ChangeStats NA = new ChangeStats(-1, -1, -1, -1, -1)
+	static final ChangeStats TOO_BIG_TO_DIFF = new ChangeStats(-2, -2, -2, -2, -2)
 
 	String fileName
 	String fileChangeType
