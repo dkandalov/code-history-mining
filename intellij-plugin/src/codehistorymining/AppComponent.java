acab5d9d7ec996919dc1e1c8911f8200162cf13b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-06-19 19:36:31 +0100

    AppComponent cleanup

--- intellij-plugin/src/codehistorymining/AppComponent.java
+++ intellij-plugin/src/codehistorymining/AppComponent.java
@@ -1,47 +1,65 @@
 package codehistorymining;
 
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.diagnostic.Logger;
 import groovy.lang.Binding;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class AppComponent implements ApplicationComponent {
+	private static final Logger LOG = Logger.getInstance("CodeHistoryMining");
 
 	@Override public void initComponent() {
 		try {
+
 			Class<?> aClass = Class.forName("plugin");
 			Method method = findMethod("run", aClass);
-			if (method == null) {
-				// TODO log
-				return;
-			}
-
-			Binding binding = new Binding();
-			binding.setVariable("event", null);
-			binding.setVariable("project", null);
-			binding.setVariable("isIdeStartup", true);
-//			binding.setVariable("pluginPath", new File(this.getClass().getClassLoader().getResource(".").toURI()).getAbsolutePath());
-			// TODO use binding
+			if (method == null) throw new IllegalStateException("Couldn't find 'plugin' class");
 
 			Constructor<?> constructor = aClass.getDeclaredConstructor(Binding.class);
-			method.invoke(constructor.newInstance(binding));
+			method.invoke(constructor.newInstance(createBinding()));
 
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace(); // TODO
+			handleException(e);
 		} catch (InvocationTargetException e) {
-			e.printStackTrace();
+			handleException(e);
 		} catch (IllegalAccessException e) {
-			e.printStackTrace();
+			handleException(e);
 		} catch (InstantiationException e) {
-			e.printStackTrace();
-//		} catch (URISyntaxException e) {
-//			e.printStackTrace();
+			handleException(e);
+		} catch (URISyntaxException e) {
+			handleException(e);
 		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
+			handleException(e);
+		} catch (IllegalStateException e) {
+			handleException(e);
 		}
+	}
+
+	private static void handleException(Exception e) {
+		LOG.error("Error during initialization", e);
+	}
+
+	private static Binding createBinding() throws URISyntaxException {
+		Binding binding = new Binding();
+		binding.setVariable("event", null);
+		binding.setVariable("project", null);
+		binding.setVariable("isIdeStartup", true);
+		binding.setVariable("pluginPath", pluginPath());
+		return binding;
+	}
+
+	private static String pluginPath() throws URISyntaxException {
+		ClassLoader classLoader = AppComponent.class.getClassLoader();
+		URL resource = classLoader.getResource("plugin.class");
+		if (resource == null) throw new IllegalStateException("Failed to find plugin.class");
+		return new File(resource.toURI()).getParent();
 	}
 
 	private static Method findMethod(String methodName, Class<?> aClass) {
