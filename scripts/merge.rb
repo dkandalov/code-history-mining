18a65faf65d9ef293dd74faa0f55413d1bbdac11
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-25 02:54:04 +0100

    extracted visualisation fixes into VisualizationsConfig module

--- scripts/merge.rb
+++ scripts/merge.rb
@@ -1,5 +1,84 @@
 #!/usr/bin/ruby
 
+module VisualizationsConfig
+  def with_change_size_chart(grouped_by = nil, moving_average = false)
+    Proc.new { |html|
+      html.gsub!(/width =.*?,/, 'width = 740,')
+      html.gsub!('var defaultTimeInterval = "monday";', "var defaultTimeInterval = \"#{grouped_by}\";") unless grouped_by.nil?
+      html.gsub!('var showMovingAverage = false;', "var showMovingAverage = #{moving_average};")
+      html.gsub!('dropDown.append("option").attr("value", "1").html("lines");', '')
+      html.gsub!('dropDown.append("option").attr("value", "2").html("characters");', '')
+      html.gsub!('return svgPos.left + margin.left', 'return margin.left')
+    }
+  end
+
+  def with_amount_of_committers(grouped_by = nil)
+    Proc.new { |html|
+      html.gsub!(/width =.*?,/, 'width = 740,')
+      unless grouped_by.nil?
+        i = ['day', 'week', 'month'].index(grouped_by)
+        html.gsub!(/var rawDataIndex = 0;/, "var rawDataIndex = #{i};")
+      end
+    }
+  end
+
+  def with_avg_amount_of_files(percentile = nil, grouped_by = nil)
+    Proc.new { |html|
+      html.gsub!(/width =.*?,/, 'width = 740,')
+      html.gsub!('var defaultPercentile = 1.0;', "var defaultPercentile = #{percentile};")
+      unless grouped_by.nil?
+        i = ['day', 'week', 'month'].index(grouped_by)
+        html.gsub!(/var rawDataIndex = 0;/, "var rawDataIndex = #{i};")
+      end
+    }
+  end
+
+  def with_files_graph(gravity = nil, min_link = nil)
+    Proc.new{ |html|
+      html.gsub!(/width =.*?,/, 'width = 800,')
+      html.gsub!(/var gravity = gravityValues.*?;/, "var gravity = gravityValues.#{gravity};") unless gravity.nil?
+      html.gsub!('var minLinkStrength = linkValuesExtent[0];', "var minLinkStrength = #{min_link};") unless min_link.nil?
+    }
+  end
+
+  def with_committers_and_files_graph(gravity = nil, min_cluster = nil, min_link = nil)
+    Proc.new{ |html|
+      html.gsub!(/width =.*?,/, 'width = 800,')
+      html.gsub!(/var gravity = gravityValues.*?;/, "var gravity = gravityValues.#{gravity};") unless gravity.nil?
+      html.gsub!('var minClusterSize = 2;', "var minClusterSize = #{min_cluster};") unless min_cluster.nil?
+      html.gsub!('var minLinkStrength = linkValuesExtent[0];', "var minLinkStrength = #{min_link};") unless min_link.nil?
+    }
+  end
+
+  def with_treemap
+    Proc.new{ |html|
+      html.gsub!(/var w =.*?,/, 'var w = 800,')
+      html.gsub!(/font:.*?;/, '')
+    }
+  end
+
+  def with_punchcard(multiplier = nil)
+    Proc.new{ |html|
+      html.gsub!(/width =.*?,/, 'width = 740,')
+      html.gsub!('var defaultCommitSizeMultiplier = 1;', 'var defaultCommitSizeMultiplier = 2;') unless multiplier.nil?
+    }
+  end
+
+  def with_histogram(percentile = nil)
+    Proc.new{ |html|
+      html.gsub!(/width =.*?,/, 'width = 740,')
+      html.gsub!(/var defaultPercentile =.*?;/, 'var defaultPercentile = 0.6;') unless percentile.nil?
+    }
+  end
+
+  def with_wordcloud(exclusions = nil)
+    Proc.new{ |html|
+      html.gsub!('normalizeWordSize(data.words);', "excludeWords([#{exclusions}]); normalizeWordSize(data.words);") unless exclusions.nil?
+    }
+  end
+end
+
+
 class String
   def end_index(s)
     index(s) + s.size
@@ -28,6 +107,7 @@ end
 def merge_visualizations(src_path, project_name, fixes_by_filename)
   do_merge(src_path, "../#{project_name}-template.html", "../#{project_name}.html", fixes_by_filename)
 end
+
 
 def do_merge(src_path, template_file, target_file, fixes_by_filename)
   remove_margin_style = Proc.new { |html|
