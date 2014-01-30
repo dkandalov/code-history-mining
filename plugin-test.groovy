58978a07fe3f1742d9c53664743284f6871d700c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-01-30 13:25:24 +0000

    WILT complexity

--- plugin-test.groovy
+++ plugin-test.groovy
@@ -4,9 +4,10 @@ import events.EventStorageTest
 import historyreader.ChangeEventsReaderGitTest
 import historyreader.CommitReaderGitTest
 import historyreader.TextCompareProcessorTest
+import historyreader.wilt.WiltTest
 import liveplugin.testrunner.IntegrationTestsRunner
 import util.TimeIteratorsTest
 
-def unitTests = [AnalysisTest, EventStorageTest, TimeIteratorsTest]
+def unitTests = [AnalysisTest, EventStorageTest, TimeIteratorsTest, WiltTest]
 def integrationTests = [TextCompareProcessorTest, CommitReaderGitTest, ChangeEventsReaderGitTest]
 IntegrationTestsRunner.runIntegrationTests(unitTests + integrationTests, project, pluginPath)
