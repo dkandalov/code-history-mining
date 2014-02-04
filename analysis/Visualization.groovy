0e44fd914a4dc36803c2cfc1df5efe752b267bc5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-02-04 08:46:00 +0000

    static imports, reordering fields

--- analysis/Visualization.groovy
+++ analysis/Visualization.groovy
@@ -2,6 +2,16 @@ package analysis
 import events.FileChangeEvent
 import groovy.transform.Immutable
 
+import static analysis.Analysis.TreeMapView.createJson_AmountOfChangeInFolders_TreeMap
+import static analysis.Analysis.commitLogAsGraph
+import static analysis.Analysis.createJson_AmountOfCommitters_Chart
+import static analysis.Analysis.createJson_AuthorConnectionsThroughChangedFiles_Graph
+import static analysis.Analysis.createJson_AverageAmountOfFilesInCommit_Chart
+import static analysis.Analysis.createJson_ChangeSize_Chart
+import static analysis.Analysis.createJson_CommitComments_WordCloud
+import static analysis.Analysis.createJson_CommitsByDayOfWeekAndTime_PunchCard
+import static analysis.Analysis.createJson_FilesInTheSameCommit_Graph
+import static analysis.Analysis.createJson_TimeBetweenCommits_Histogram
 import static http.AllTemplates.*
 
 @Immutable
@@ -10,60 +20,59 @@ class Visualization {
 	final Closure<String> generate
 
 	static changeSizeChart = new Visualization("Change Size Chart", { Context context ->
-		def json = Analysis.createJson_ChangeSize_Chart(context.events, context.checkIfCancelled)
+		def json = createJson_ChangeSize_Chart(context.events, context.checkIfCancelled)
 		changeSizeChartTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
 	static amountOfCommittersChart = new Visualization("Amount Of Committers Chart", { Context context ->
-		def json = Analysis.createJson_AmountOfCommitters_Chart(context.events, context.checkIfCancelled)
+		def json = createJson_AmountOfCommitters_Chart(context.events, context.checkIfCancelled)
 		amountOfCommittersChartTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
 	static amountOfFilesInCommitChart = new Visualization("Amount Of Files In Commit Chart", { Context context ->
-		def json = Analysis.createJson_AverageAmountOfFilesInCommit_Chart(context.events, context.checkIfCancelled)
+		def json = createJson_AverageAmountOfFilesInCommit_Chart(context.events, context.checkIfCancelled)
 		amountOfFilesInCommitChartTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
+	static filesInTheSameCommitGraph = new Visualization("Files In The Same Commit Graph", { Context context ->
+		def json = createJson_FilesInTheSameCommit_Graph(context.events, context.checkIfCancelled)
+		filesInTheSameCommitGraphTemplate.fillData(json).fillProjectName(context.projectName).text
+	})
+
+	static committersChangingSameFilesGraph = new Visualization("Committers Changing Same Files Graph", { Context context ->
+		def json = createJson_AuthorConnectionsThroughChangedFiles_Graph(context.events, context.checkIfCancelled)
+		committersChangingSameFilesGraphTemplate.fillData(json).fillProjectName(context.projectName).text
+	})
+
 	static amountOfCommitsTreemap = new Visualization("Amount Of Commits Treemap", { Context context ->
-		def json = Analysis.TreeMapView.createJson_AmountOfChangeInFolders_TreeMap(context.events, context.checkIfCancelled)
+		def json = createJson_AmountOfChangeInFolders_TreeMap(context.events, context.checkIfCancelled)
 		amountOfCommitsTreemapTemplate.fillData(json).fillProjectName(context.projectName).text
 		// TODO try sunburst layout? (http://bl.ocks.org/mbostock/4063423)
 	})
 
-	static filesInTheSameCommitGraph = new Visualization("Files In The Same Commit Graph", { Context context ->
-		def json = Analysis.createJson_FilesInTheSameCommit_Graph(context.events, context.checkIfCancelled)
-		filesInTheSameCommitGraphTemplate.fillData(json).fillProjectName(context.projectName).text
-	})
-
-	static commitLogAsGraph = new Visualization("Commit Log As Graph", { Context context ->
-		def json = Analysis.commitLogAsGraph(context.events, context.checkIfCancelled)
-		commitLogAsGraphTemplate.fillData(json).fillProjectName(context.projectName).text
-	})
-
-	static committersChangingSameFilesGraph = new Visualization("Committers Changing Same Files Graph", { Context context ->
-		def json = Analysis.createJson_AuthorConnectionsThroughChangedFiles_Graph(context.events, context.checkIfCancelled)
-		committersChangingSameFilesGraphTemplate.fillData(json).fillProjectName(context.projectName).text
-	})
-
 	static commitTimePunchcard = new Visualization("Commit Time Punchcard", { Context context ->
-		def json = Analysis.createJson_CommitsByDayOfWeekAndTime_PunchCard(context.events, context.checkIfCancelled)
+		def json = createJson_CommitsByDayOfWeekAndTime_PunchCard(context.events, context.checkIfCancelled)
 		commitTimePunchcardTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
 	static timeBetweenCommitsHistogram = new Visualization("Time Between Commits Histogram", { Context context ->
-		def json = Analysis.createJson_TimeBetweenCommits_Histogram(context.events, context.checkIfCancelled)
+		def json = createJson_TimeBetweenCommits_Histogram(context.events, context.checkIfCancelled)
 		timeBetweenCommitsHistogramTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
 	static commitMessageWordCloud = new Visualization("Commit Messages Word Cloud", { Context context ->
-		def json = Analysis.createJson_CommitComments_WordCloud(context.events, context.checkIfCancelled)
+		def json = createJson_CommitComments_WordCloud(context.events, context.checkIfCancelled)
 		commitMessageWordCloudTemplate.fillData(json).fillProjectName(context.projectName).text
 	})
 
+
 	static all = new Visualization("All Visualizations", { Context context ->
 		def templates = [
-				changeSizeChartTemplate.fillData(Analysis.createJson_ChangeSize_Chart(context.events, context.checkIfCancelled)),
-				amountOfCommittersChartTemplate.fillData(Analysis.createJson_AmountOfCommitters_Chart(context.events, context.checkIfCancelled))
+				changeSizeChartTemplate.fillData(createJson_ChangeSize_Chart(context.events, context.checkIfCancelled)),
+				amountOfCommittersChartTemplate.fillData(createJson_AmountOfCommitters_Chart(context.events, context.checkIfCancelled)),
+				amountOfFilesInCommitChartTemplate.fillData(createJson_AverageAmountOfFilesInCommit_Chart(context.events, context.checkIfCancelled)),
+//				filesInTheSameCommitGraphTemplate.fillData(createJson_FilesInTheSameCommit_Graph(context.events, context.checkIfCancelled)),
+//				committersChangingSameFilesGraphTemplate.fillData(createJson_AuthorConnectionsThroughChangedFiles_Graph(context.events, context.checkIfCancelled))
 		]
 
 		def template = allVisualizationsTemplate.fillProjectName(context.projectName.capitalize())
@@ -89,6 +98,12 @@ class Visualization {
 		}
 		template.text
 	})
+
+	static commitLogAsGraph = new Visualization("Commit Log As Graph", { Context context ->
+		def json = commitLogAsGraph(context.events, context.checkIfCancelled)
+		commitLogAsGraphTemplate.fillData(json).fillProjectName(context.projectName).text
+	})
+
 
 
 	static class Context {
