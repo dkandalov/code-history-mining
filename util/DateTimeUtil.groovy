60e27e7ced11d520dff02a8fdf76461fcc73d726
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-03-09 15:59:03 +0000

    added TimeInterval to DateTimeUtil

--- util/DateTimeUtil.groovy
+++ util/DateTimeUtil.groovy
@@ -33,37 +33,94 @@ class DateTimeUtil {
 	}
 
 	static Date floorToDay(Date date, TimeZone timeZone = TimeZone.default) {
-		Calendar.getInstance(timeZone).with{
-			time = date
-			set(MILLISECOND, 0)
-			set(SECOND, 0)
-			set(MINUTE, 0)
-			set(HOUR_OF_DAY, 0)
-			time
-		}
+		oneDay.floor(date, timeZone)
 	}
 
 	static Date floorToWeek(Date date, TimeZone timeZone = TimeZone.default) {
-		Calendar.getInstance(timeZone).with{
-			time = date
-			set(MILLISECOND, 0)
-			set(SECOND, 0)
-			set(MINUTE, 0)
-			set(HOUR_OF_DAY, 0)
-			set(DAY_OF_WEEK, MONDAY)
-			time
-		}
+		oneWeek.floor(date, timeZone)
 	}
 
 	static Date floorToMonth(Date date, TimeZone timeZone = TimeZone.default) {
-		Calendar.getInstance(timeZone).with{
-			time = date
-			set(MILLISECOND, 0)
-			set(SECOND, 0)
-			set(MINUTE, 0)
-			set(HOUR_OF_DAY, 0)
-			set(DAY_OF_MONTH, 1)
-			time
+		oneMonth.floor(date, timeZone)
+	}
+
+	static final oneDay = days(1)
+	static final oneWeek = weeks(1)
+	static final oneMonth = months(1)
+
+	interface TimeInterval {
+		Date floor(Date date, TimeZone timeZone)
+		Date next(Date date, TimeZone timeZone)
+	}
+
+	static TimeInterval days(int amount = 1) {
+		new TimeInterval() {
+			@Override Date floor(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = date
+					set(MILLISECOND, 0)
+					set(SECOND, 0)
+					set(MINUTE, 0)
+					set(HOUR_OF_DAY, 0)
+					time
+				}
+			}
+
+			@Override Date next(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = floor(date, timeZone)
+					add(DAY_OF_MONTH, amount)
+					time
+				}
+			}
+		}
+	}
+
+	static TimeInterval weeks(int amount = 1) {
+		new TimeInterval() {
+			@Override Date floor(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = date
+					set(MILLISECOND, 0)
+					set(SECOND, 0)
+					set(MINUTE, 0)
+					set(HOUR_OF_DAY, 0)
+					set(DAY_OF_WEEK, MONDAY)
+					time
+				}
+			}
+
+			@Override Date next(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = floor(date, timeZone)
+					add(WEEK_OF_YEAR, amount)
+					time
+				}
+			}
+		}
+	}
+
+	static TimeInterval months(int amount = 1) {
+		new TimeInterval() {
+			@Override Date floor(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = date
+					set(MILLISECOND, 0)
+					set(SECOND, 0)
+					set(MINUTE, 0)
+					set(HOUR_OF_DAY, 0)
+					set(DAY_OF_MONTH, 1)
+					time
+				}
+			}
+
+			@Override Date next(Date date, TimeZone timeZone = TimeZone.default) {
+				Calendar.getInstance(timeZone).with{
+					time = floor(date, timeZone)
+					add(MONTH, amount)
+					time
+				}
+			}
 		}
 	}
 
