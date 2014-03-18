6f9b00844ed9dd0b3803f8cfe5d8831859d787e3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-03-18 23:25:12 +0000

    removed unused code

--- analysis/templates/html/code-history.js
+++ analysis/templates/html/code-history.js
@@ -374,39 +374,6 @@ QuickFind.prototype.rootOf = function(p) {
 };
 
 
-function movingAverage(data, timeInterval, getValue, period) {
-	if (data.length < 2) return [];
-
-	period = (period == null ? Math.round(data.length / 10) : period);
-	if (period < 2) return [];
-
-	var firstDate = data[0].date;
-	var lastDatePlusOne = timeInterval.offset(data[data.length - 1].date, 1);
-	var allDates = timeInterval.range(firstDate, lastDatePlusOne);
-	if (allDates.length < period) return [];
-
-	data = valuesForEachDate(data, allDates, function(d) { return d.date; }, getValue);
-
-	var mean = d3.mean(allDates.slice(0, period), function(date){ return data[date]; });
-	var result = [{date: allDates[period - 1], mean: mean}];
-
-	for (var i = period; i < allDates.length; i++) {
-		var date = allDates[i];
-		var dateToExclude = allDates[i - period];
-		mean += (data[date] - data[dateToExclude]) / period;
-		result.push({date: date, mean: mean});
-	}
-
-	return result;
-}
-
-function valuesForEachDate(data, datesRange, getDate, getValue) {
-	var result = {};
-	datesRange.forEach(function(date) { result[date] = 0; });
-	data.forEach(function(d) { result[getDate(d)] = getValue(d); });
-	return result;
-}
-
 // this is intended for projector in case default colors are too pale to see
 function enableDarkColorsShortcut() {
 	document.onkeydown = function(e) {
