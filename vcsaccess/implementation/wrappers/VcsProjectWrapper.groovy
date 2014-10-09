5929d247699974caf809f051d16b3ff1b3cd3b85
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-10-09 21:58:04 +0100

    compilation fix in VcsProjectWrapper

--- vcsaccess/implementation/wrappers/VcsProjectWrapper.groovy
+++ vcsaccess/implementation/wrappers/VcsProjectWrapper.groovy
@@ -6,16 +6,11 @@ import vcsreader.VcsProject
 import vcsreader.VcsRoot
 
 class VcsProjectWrapper extends VcsProject {
-    private final IJProject project
-    private final VcsAccessLog log
-
     VcsProjectWrapper(IJProject project, List<IJVcsRoot> roots, String commonVcsRoot, VcsAccessLog log) {
-        super(convert(roots, project, commonVcsRoot))
-        this.project = project
-        this.log = log
+        super(convertRoots(project, roots, commonVcsRoot, log))
     }
 
-    private static List<VcsRoot> convert(Collection<IJVcsRoot> vcsRoots, IJProject project, String commonVcsRoot) {
+    private static List<VcsRoot> convertRoots(IJProject project, List<IJVcsRoot> vcsRoots, String commonVcsRoot, VcsAccessLog log) {
         vcsRoots.collect { new VcsRootWrapper(project, it, commonVcsRoot, log) }
     }
 }
