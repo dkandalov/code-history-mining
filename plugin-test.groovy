c02258ee1bd7daaeffb40bd282447a5f48027f67
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-08-09 18:32:48 +0100

    ide files

--- plugin-test.groovy
+++ plugin-test.groovy
@@ -1,11 +1,11 @@
 import codemining.historystorage.HistoryGrabberConfigTest
-import liveplugin.testrunner.IntegrationTestsRunner
-import codemining.plugin.GroovyStubber
 import codemining.plugin.CodeMiningPluginTest
-import codemining.vcsaccess.implementation.MiningCommitReader_GitIntegrationTest
+import codemining.plugin.GroovyStubber
 import codemining.vcsaccess.implementation.IJCommitReaderGitTest
+import codemining.vcsaccess.implementation.MiningCommitReader_GitIntegrationTest
+import liveplugin.testrunner.IntegrationTestsRunner
 
-// add-to-classpath $HOME/Library/Application Support/IntelliJIdea14/live-plugins/code-history-mining/build/classes/main/
+// add-to-classpath $HOME/Library/Application Support/IntelliJIdea14/live-plugins/code-history-miner/build/classes/main/
 // add-to-classpath $PLUGIN_PATH/lib/codemining/core/1.0/core-1.0.jar
 // add-to-classpath $PLUGIN_PATH/lib/codemining/vcs-reader/1.0/vcs-reader-1.0.jar
 // add-to-classpath $PLUGIN_PATH/lib/liveplugin/live-plugin/0.5.2 beta/live-plugin-0.5.2 beta.jar
