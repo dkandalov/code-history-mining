a3cff15726f3d93fc6d1497ed4f32fdec1bf1172
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-02-29 23:51:51 +0000

    default content for newly created script

--- src/main/codehistoryminer/plugin/historystorage/ScriptStorage.groovy
+++ src/main/codehistoryminer/plugin/historystorage/ScriptStorage.groovy
@@ -1,5 +1,6 @@
 package codehistoryminer.plugin.historystorage
 
+import codehistoryminer.core.lang.Misc
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.io.FileUtilRt
 
@@ -15,14 +16,27 @@ class ScriptStorage {
 		FileUtil.createDirectory(scriptsFolder)
 
 		def scriptFile = new File(scriptsFolder.absolutePath + File.separator + fileName)
-		def wasCreated = FileUtil.createIfDoesntExist(scriptFile)
-		if (!wasCreated) throw new FileNotFoundException(scriptFile.absolutePath)
-
+		def isNewFile = !scriptFile.exists()
+		if (isNewFile) {
+			def wasCreated = FileUtil.createIfDoesntExist(scriptFile)
+			if (!wasCreated) throw new FileNotFoundException(scriptFile.absolutePath)
+			scriptFile.write(newScriptContent(), Misc.UTF8.name())
+		}
 		scriptFile
 	}
 
 	boolean isScriptFile(String filePath) {
 		FileUtilRt.getExtension(filePath) == "groovy" &&
 		FileUtil.isAncestor(new File(basePath), new File(filePath), true)
+	}
+
+	private static String newScriptContent() {
+		"""
+// To run the script use alt+shift+E (or "Run Code History Script" in editor context menu).
+// For more details about scripts and examples see GitHub wiki
+// https://github.com/dkandalov/code-history-mining/wiki/Code-History-Script-API.
+
+data.size()
+"""
 	}
 }
